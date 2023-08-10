59d94427c1da6e79fe3de05b29bbbc8dea6b27de
bossSteve03 <stotev0@gmail.com>
2023-08-10 22:54:50 +0100

    first commit plus a few files already done

---
+++ controllers/email.Controller.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+const Email = erquire('../models/Email');
+require(dotenv).config();
+
+const emailController = {
+  sendEmail: async (req, res) => {
+    const { email, heading, body } = req.body;
+    const emailData = new Email(email, heading, body);
+
+    try {
+      const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+
+      const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: 'stotev0@gmail.com',
+        subject: emailData.heading,
+        text: emailData.body,
+      };
+
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({message: 'Email sent successfully'});
+    } catch (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ error: 'An error occurred while sending the email' });
+    }
+  },
+};
+
+module.exports = emailController;
---
+++ models/Email.js
@@ -0,0 +1,9 @@
+class Email {
+  constructor(email, heading, body) {
+    this.email = email;
+    this.heading = heading;
+    this.body = body;
+  }
+}
+
+module.exports = Email;
